388cb678662d2da1efd01b2f7a27a4e130496c0d
Luffytaro22 <manuelsanchez059@gmail.com>
2023-04-14 11:07:18 -0500

    Completed the calculator function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	if(a == []){
+    return 0;
+  }else{
+    return a.reduce((sum,x) => sum + x,0);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let numMultiply = a[0];
+  for(let i = 1; i < a.length; i++){
+    numMultiply *= a[i];
+  }
+  return numMultiply;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a == 0){
+    return 1;
+  }else{
+    let numFact = a;
+    for(let i = a-1; i > 0; i--){
+      numFact *= i;
+    }
+    return numFact;
+  }
 };
 
 // Do not edit below this line
