32296ed6ca75c8b9dccf08be0fa040edb78aed00
Luffytaro22 <manuelsanchez059@gmail.com>
2023-04-14 11:26:43 -0500

    Completed fibonnaci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(a <= 0) return 'OOPS';
+    let fibonnaciSequence = [1,1];
+    for(let i = 2; i < a; i++){
+        fibonnaciSequence[i] = fibonnaciSequence[i-1] + fibonnaciSequence[i-2];
+    }
+    return fibonnaciSequence[a-1];
 };
 
 // Do not edit below this line
