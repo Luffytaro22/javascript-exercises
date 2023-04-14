26ff398a6068325ac22c3cd0af139e6473db5f8f
Luffytaro22 <manuelsanchez059@gmail.com>
2023-04-14 11:59:13 -0500

    Completed finTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const actualYear = new Date().getFullYear();
+    const copyArray = [...array];
+    let oldest = 0;
+    let oldestIndex = 0;
+    array.map(function(Object, index){
+        if(Object.hasOwnProperty('yearOfDeath')){
+            if(Object['yearOfDeath'] - Object['yearOfBirth'] > oldest){
+                oldest = Object['yearOfDeath'] - Object['yearOfBirth'];
+                oldestIndex = index;
+            }
+            return Object['yearOfDeath'] - Object['yearOfBirth'];
+        }else{
+            if(actualYear - Object['yearOfBirth'] > oldest){
+                oldest = actualYear - Object['yearOfBirth'];
+                oldestIndex = index;
+            }
+            return actualYear - Object['yearOfBirth'];
+        }
+    });
+    return copyArray[oldestIndex];
 };
 
 // Do not edit below this line
