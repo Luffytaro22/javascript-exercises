d1f25fd278f24c99eabc456bb045e618d07365b6
Luffytaro22 <manuelsanchez059@gmail.com>
2023-04-14 09:30:52 -0500

    Completed the reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    string.split('');
+    let reverse = [];
+    for(let i = string.length-1; i >= 0; i--){
+        reverse.push(string[i]);
+    }
+    return reverse.join('');
 };
 
 // Do not edit below this line
